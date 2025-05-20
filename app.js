b69fe531a0cb8f37f826234a637a57f17cd2ddae
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-05-20 19:44:33 +0530

    completed the how many items left functionality, made the delete button function, now we can add tasks

--- app.js
+++ app.js
@@ -1,40 +1,66 @@
-let tasks = [
-    {id:1,task:"completed project",completed:false},
-    {id:2,task:"learn something",completed:false},
-    {id:3,task:"clean room",completed:false},
-]
+let tasks = []
+let numberofTask = 0
 let todoList = document.querySelector(".todo-list")
-let todoListHTML = ''
-tasks.forEach((ele) => {
-    todoListHTML += `
-    <li class="tasks">
-        <div class="task">
-            <button class="toggle-completed ${ele['completed'] ? 'completed' : ''}" type="button"><img src="images/icon-check.svg"></button>
-            <p>${ele['task']}</p>
-        </div>
-        <button class="delete-todo" type="button"><img src="images/icon-cross.svg"></button>
-    </li>
-    `
-})
-todoList.innerHTML = todoListHTML
-/* <li class="tasks"> */
-    // <div class="task">
-        // <button class="toggle-completed" type="button"><img src="images/icon-check.svg"></button>
-        // <p>Hello</p>
-    // </div>
-    // <button class="delete-todo" type="button"><img src="images/icon-cross.svg"></button>
-// </li>
+const inputField = document.querySelector("input")
 
+function itemsLeft(value){
+    let itemleftPara = document.querySelector(".itemsLeft")
+    itemleftPara.innerHTML = `${value} items left`
+}
 
+function toggleButon(){
+    let completedTask = document.querySelectorAll('.task button')
 
-let completedTask = document.getElementsByClassName('toggle-completed')
-Object.keys(completedTask).forEach((key) =>{
-    completedTask[key].addEventListener("click",() =>{
-        let ele = completedTask[key].classList
-        if (!ele.contains('completed')){
-            ele.add("completed")
-        } else{
-            ele.remove("completed")
-        }
+    Object.keys(completedTask).forEach((key) =>{
+        completedTask[key].addEventListener("click",() =>{
+            let ele = completedTask[key].classList
+            if (!ele.contains('completed')){
+                ele.add("completed")
+                tasks[key]['completed'] = true
+            } else{
+                ele.remove("completed")
+                tasks[key]['completed'] = false
+            }
+        })
     })
+}
+
+function deleteButton() {
+    let deleteTask = document.querySelectorAll(".delete-todo")
+    Object.keys(deleteTask).forEach((key) =>{
+        deleteTask[key].addEventListener("click",() =>{
+            tasks.splice(key,1)
+            renderTask()
+        })
+    })
+}
+
+function renderTask() {
+    let todoListHTML = ''
+    tasks.forEach((ele) => {
+        todoListHTML += `
+        <li class="tasks">
+            <div class="task">
+                <button class="toggle-completed ${ele['completed'] ? 'completed' : ''}" type="button"><img src="images/icon-check.svg"></button>
+                <p>${ele['task']}</p>
+            </div>
+            <button class="delete-todo" type="button"><img src="images/icon-cross.svg"></button>
+        </li>
+        `
+    })
+    todoList.innerHTML = todoListHTML
+    toggleButon()
+    deleteButton()
+}
+
+
+inputField.addEventListener("keydown",(event) => {
+    if (event.key == "Enter"){
+        let newTask = {id:numberofTask,task:inputField.value,completed:false}
+        numberofTask += 1
+        tasks.push(newTask)
+        renderTask()
+        itemsLeft(numberofTask)
+    }
 })
+
