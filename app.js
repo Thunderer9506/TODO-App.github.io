235bae65e4c36c481cecb944bfdea8240b93f009
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-05-20 12:12:13 +0530

    completd the task done and undone functionality, task rendering functionality

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-let tasks = document.getElementsByClassName("task")
+let tasks = [
+    {id:1,task:"completed project",completed:false},
+    {id:2,task:"learn something",completed:false},
+    {id:3,task:"clean room",completed:false},
+]
+let todoList = document.querySelector(".todo-list")
+let todoListHTML = ''
+tasks.forEach((ele) => {
+    todoListHTML += `
+    <li class="tasks">
+        <div class="task">
+            <button class="toggle-completed ${ele['completed'] ? 'completed' : ''}" type="button"><img src="images/icon-check.svg"></button>
+            <p>${ele['task']}</p>
+        </div>
+        <button class="delete-todo" type="button"><img src="images/icon-cross.svg"></button>
+    </li>
+    `
+})
+todoList.innerHTML = todoListHTML
+/* <li class="tasks"> */
+    // <div class="task">
+        // <button class="toggle-completed" type="button"><img src="images/icon-check.svg"></button>
+        // <p>Hello</p>
+    // </div>
+    // <button class="delete-todo" type="button"><img src="images/icon-cross.svg"></button>
+// </li>
 
 
-Object.keys(tasks).forEach(key => {
-    tasks[key]['children'][0].addEventListener("click", (element) => {
-        let completeButton = tasks[key]['children'][0]
-        let buttonImage = completeButton['children'][0]
-        console.log(completeButton.classList[0]);
-        
-        if(completeButton.classList[0]){
-            completeButton.classList.add('complete')
-        } 
-        
+
+let completedTask = document.getElementsByClassName('toggle-completed')
+Object.keys(completedTask).forEach((key) =>{
+    completedTask[key].addEventListener("click",() =>{
+        let ele = completedTask[key].classList
+        if (!ele.contains('completed')){
+            ele.add("completed")
+        } else{
+            ele.remove("completed")
+        }
     })
-    
-});
-
+})
